0912e9b103caaa6d03e4a6ca5e16523ab19de228
ArisPodotas <a.Podotas@gmail.com>
2025-10-02 15:39:39 +0300

    Fixed the monitor scale script and changed caps to escape

--- scripts/scale-down.sh
+++ scripts/scale-down.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Pick the monitor you want to scale (e.g. eDP-1 or HDMI-A-1)
-# In case the name isnt right
-# MONITOR=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .name')
+#Pick the monitor you want to scale (e.g. eDP-1 or HDMI-A-1)
+#In case the name isnt right
+MONITOR=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .name')
 MONITOR="eDP-1"
 
 # current scale, resolution, position
@@ -12,12 +12,9 @@ HEIGHT=$(hyprctl -j monitors | jq -r ".[] | select(.name==\"$MONITOR\") | .heigh
 POSX=$(hyprctl -j monitors | jq -r ".[] | select(.name==\"$MONITOR\") | .x")
 POSY=$(hyprctl -j monitors | jq -r ".[] | select(.name==\"$MONITOR\") | .y")
 
-
-# remove 0.1
-NEW=$(echo "scale=2; $CURSCALE - 0.1" | bc)
-
-# format to 1 decimal place (e.g., 1.10 → 1.1)
-NEW=$(printf "%.1f" "$NEW")
+NEW=$(echo "scale=2; $CURSCALE - 0.25" | bc)
 
 # apply with preserved resolution + position
 hyprctl keyword monitor "$MONITOR,$RESx$HEIGHT@0,$POSXx$POSY,$NEW"
+
+notify-send "Hyprland" "Monitor $MONITOR scale set to $NEW"
--- scripts/scale-up.sh
+++ scripts/scale-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Pick the monitor you want to scale (e.g. eDP-1 or HDMI-A-1)
-# In case the name isnt right
+#Pick the monitor you want to scale (e.g. eDP-1 or HDMI-A-1)
+#In case the name isnt right
 # MONITOR=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .name')
 MONITOR="eDP-1"
 
@@ -13,10 +13,12 @@ POSX=$(hyprctl -j monitors | jq -r ".[] | select(.name==\"$MONITOR\") | .x")
 POSY=$(hyprctl -j monitors | jq -r ".[] | select(.name==\"$MONITOR\") | .y")
 
 # add 0.1, force one decimal
-NEW=$(echo "scale=2; $CURSCALE + 0.1" | bc)
+NEW=$(echo "scale=2; $CURSCALE + 0.25" | bc)
 
 # format to 1 decimal place (e.g., 1.10 → 1.1)
-NEW=$(printf "%.1f" "$NEW")
+# NEW=$(printf "%.1f" "$NEW")
 
 # apply with preserved resolution + position
 hyprctl keyword monitor "$MONITOR,$RESx$HEIGHT@0,$POSXx$POSY,$NEW"
+
+notify-send "Hyprland" "Monitor $MONITOR scale set to $NEW"
