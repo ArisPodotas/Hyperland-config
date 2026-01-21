74381d1ed8343dfede0f0d3da0ae7243f26c7021
ArisPodotas <a.Podotas@gmail.com>
2026-01-21 07:21:04 +0200

    Changed an option about opacity scaling

--- scripts/tr-up.sh
+++ scripts/tr-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 current=$(hyprctl getoption decoration:active_opacity -j | jq -r '.float')
-new=$(echo "$current + 0.1" | bc)
+new=$(echo "$current + 0.05" | bc)
 if (( $(echo "$new > 1.0" | bc -l) )); then
     new=1.0
 fi
@@ -11,7 +11,7 @@ hyprctl keyword decoration:active_opacity "$new"
 notify-send "Hyprland" "Opacity set to $new"
 
 current=$(hyprctl getoption decoration:inactive_opacity -j | jq -r '.float')
-new=$(echo "$current + 0.1" | bc)
+new=$(echo "$current + 0.05" | bc)
 if (( $(echo "$new > 1.0" | bc -l) )); then
     new=1.0
 fi
@@ -19,10 +19,9 @@ fi
 hyprctl keyword decoration:inactive_opacity "$new"
 
 current=$(hyprctl getoption decoration:fullscreen_opacity -j | jq -r '.float')
-new=$(echo "$current + 0.1" | bc)
+new=$(echo "$current + 0.05" | bc)
 if (( $(echo "$new > 1.0" | bc -l) )); then
     new=1.0
 fi
 
 hyprctl keyword decoration:fullscreen_opacity "$new"
-
